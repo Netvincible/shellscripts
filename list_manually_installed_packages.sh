3071a4d6444bba8bbf8820957307aa75716a915f
netvincible <netvincible@gmail.com>
2025-08-27 22:52:03 +0530

    list_manually_installed_packages.sh corrected

--- list_manually_installed_packages.sh
+++ list_manually_installed_packages.sh
@@ -1,3 +1,5 @@
-for package in $(pacman -Qe); do
-	echo $(pacman -Qi "$package" | grep "Installed Size")
+echo "" > list.txt
+for package in $(pacman -Qe | awk '{print $1}'); do
+	echo "$(pacman -Qi "$package" | grep "Installed Size") : $package" >> list.txt
 done
+sort -k5,5 -k4,4g list.txt
--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-remote=$(ps aux | awk '{print $1;exit}')
-if [ -z "$remote" ];
-then
-	echo "true"
-fi
+
+BUFFETT="Life is like a snowball. The important thing is finding wet snow and a really long hill."
+
+    # write your code here
+    ISAY=$BUFFETT
+    change1=${ISAY[@]/snow/foot}
+    change2=${change1[@]//snow/}
+    change3=${change2[@]/finding/getting}
+    loc=`expr index "$change3" 'w'`
+echo $loc
+ISAY=${change3::$loc+2}
+echo $ISAY
