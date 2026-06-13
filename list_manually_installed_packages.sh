44e2564dfb64d4d57ba5f4ccf80c2664bf240337
netvincible <netvincible@gmail.com>
2026-06-13 12:22:03 +0530

    removed list_manually_installed_packages.sh

--- list_manually_installed_packages.sh
+++
@@ -1,5 +0,0 @@
-echo "" > list.txt
-for package in $(pacman -Qe | awk '{print $1}'); do
-	echo "$(pacman -Qi "$package" | grep "Installed Size") : $package" >> list.txt
-done
-sort -k5,5 -k4,4g list.txt
